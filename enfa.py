596b7b17de0b722dfab77bf12a61c72483920078
d3ni <deni1myftiu@gmail.com>
2020-02-20 15:12:20 +0100

    made the file a script

--- enfa.py
+++ enfa.py
@@ -1,41 +1,29 @@
 #!/usr/bin/env python3.7
 import json
+import sys
 from pprint import pprint
-
-''' 
-A normal epsilon nfa.
-{'alphabet': ['a', 'b', 'epsilon'],
- 'hops': {'1': {'epsilon': ['1', '2', '3']},
-          '2': {'a': [4], 'epsilon': []},
-          '3': {'b': [5], 'epsilon': []},
-          '4': {'epsilon': ['4', '6']},
-          '5': {'epsilon': ['5', '6']},
-          '6': {'epsilon': ['6', '7']},
-          '7': {'a': [8], 'epsilon': []},
-          '8': {'epsilon': ['8', '9']}},
- states': ['1', '2', '3', '4', '5', '6', '7', '8', '9']}
-'''
-inp =input("Isert Json file: ")
-with open(inp, 'rb')as jfile:
-    data = json.load(jfile)
 
 def goes_with_epsilon(state_list):
     ret=[]
     for s in state_list:
+        #join dhe jumps with epsilon
         ret += hops[s]['epsilon'] 
+    #returns a list of where the state can go with epsilon
     return list(set(ret))
 
 def epsilon_closure(llist):
     ret = []
+    #where are we going with each character in the alphabet
     for character in data['alphabet'][:-1]:
         cr=[]
+        #do that for every state in the given list
         for state in llist:
             if character in hops[state].keys():
                 cr+=hops[state][character]
         cr = goes_with_epsilon(cr)
         ret.append(cr)
+    #returns the epsilon closure of a list of states 
     return ret
-
 
 def find_endstates(state_list):
     endstates = []
@@ -47,9 +35,20 @@ def find_endstates(state_list):
     return endstates
 
 if __name__=='__main__':
+    #take the filename as argument
+    inp = sys.argv[1]
+
+    with open(inp, 'rb')as jfile:
+        data = json.load(jfile)
+
+    #we only need the hops dictionary for the algorithm
     hops = data['hops']
-    nfa = {'alphabet': data['alphabet'][:-1],'states': data['states'],"endstate":data['endstate'], "beginstate":data['beginstate']}
+    #prepare the json for the nfa
+    nfa = {'alphabet': data['alphabet'][:-1],'states': data['states'],
+            "endstate":data['endstate'], "beginstate":data['beginstate']}
     nfa_hops = {}
+
+    #runs the algorithm for the given json e-nfa
     for state in hops.keys():
         first_step = goes_with_epsilon(hops[state]['epsilon'])
         nfa_state = epsilon_closure(first_step)
@@ -57,6 +56,8 @@ if __name__=='__main__':
         for i,character in enumerate(nfa['alphabet']):
             chars.update({character: nfa_state[i]})
         nfa_hops.update({state : chars})
+
+    #print the nfa for later use
     nfa.update({"hops":nfa_hops})
     nfa['endstate'] = find_endstates(hops.keys())
     pprint(nfa)
