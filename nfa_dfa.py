c95dde92372de842c92b7812279ada2e4599a43d
d3ni <deni1myftiu@gmail.com>
2020-02-21 01:55:49 +0100

    Finished this stupid conversion to dfa i am fucking dying for sleep

--- nfa_dfa.py
+++ nfa_dfa.py
@@ -3,51 +3,63 @@ import json
 import sys
 from pprint import pprint
 
-with open('nfa.json', 'rb')as jfile:
-    data = json.load(jfile)
-'''
-{"alphabet": ["a", "b"],
- "beginstate": ["A"],
- "endstate": ["B"],
- "hops": {"A": {"a": ["A", "B"], "b": ["A"]},
-          "B": {"a": [], "b": []}},
- "states": ["A", "B"]
- }
-'''
 def union(state_list):
     ret = []
     for character in data['alphabet']:
         cr=[]
         for state in state_list:
-            if character in hops[state].keys():
-                cr.extend(hops[state][character])
+            cr.extend(hops[state][character])
         ret.append(sorted(cr))
-    #returns the epsilon closure of a list of states 
     return ret
 
-def put_unseen_in_stack(state_list, stack):
-    for state in state_list:
-        if state in seen:
-            stack.append(state)
-        
-#pprint(data) 
-hops = data['hops']
-begin_state = data['beginstate']
-seen= [['A']]
-stack = []
-#initial setup
-for el in hops[begin_state[0]].values():
-    if el != begin_state and el not in stack:
-        stack.append(el)
-        seen.append(el)
+def find_final_states(seen):
+    final_states = []
+    for el in seen:
+        if final_state in el:
+            final_states.append(''.join(el))
+    return final_states
 
-print('stack',stack)
-print(max(map(len, seen)))
-while len(stack) == 0:
-    for i,el in enumerate(stack):
-        newstates = union(el)     
-        put_unseen_in_stack(newstates, stack)
-        stack.pop(i)
-    print('new',newstates)
-    print('seen',seen)
-    print('stack',stack)
+def update_stack(newstates, stack, seen):
+    for state in newstates:
+        if state not in stack and state not in seen:
+            stack.append(sorted(state))
+            seen.append(sorted(state))
+
+if __name__=='__main__':
+    #take the filename as argument
+    try:
+        inp = sys.argv[1]
+    except:
+        print('No File Input')
+        exit(1)
+
+    with open(inp, 'rb')as jfile:
+        data = json.load(jfile)
+
+    hops = data['hops']
+    begin_state = data['beginstate']
+    final_state = data['endstate'][0]
+    seen= []
+    stack = []
+    stack.append(begin_state)
+    seen.append(begin_state)
+
+    dfa = {'alphabet':data['alphabet'],'states': data['states'],
+        "beginstate":data['beginstate']}
+    dfa_hops = {}
+
+    while len(stack) > 0:
+        print('before pop {}'.format(stack))
+        newstates = union(stack[0])     
+        update_stack(newstates, stack, seen)
+        chars = {}
+        el = stack.pop(0)
+        print('after pop {}'.format(stack))
+        for i,character in enumerate(data['alphabet']):
+            chars.update({character: ''.join(newstates[i])})
+        dfa_hops.update({''.join(el):chars})
+
+    final_states = find_final_states(seen)
+    dfa['endstate']=final_states
+    dfa['hops']=dfa_hops
+    pprint(dfa)
